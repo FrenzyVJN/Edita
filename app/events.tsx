cfc000aab6f0f97532481fb57a306c03ae7c0a92
FrenzyVJN <svijayan01@gmail.com>
2023-11-30 13:46:20 +0530

    desktop updated

--- app/events.tsx
+++ app/events.tsx
@@ -1,9 +1,23 @@
+import React from 'react';
+
 function Events() {
-    return ( 
-        <main className="flex justify-center items-center h-screen">
-            <a href="/events" id='events' className="text-white">Coming soon...</a>
-        </main>
-     );
+  return (
+    <main id="events" className="flex flex-col justify-center items-center h-screen animate__animated animate__fadeIn">
+      <h1 className="text-4xl font-bold text-blue-300 mb-6 animate__animated animate__fadeInDown">Upcoming Events</h1>
+      <div className="event-card animate__animated animate__fadeInUp">
+        <a href='/events'>
+        <h2 className="text-2xl font-semibold mb-2 text-white">EditaCTF</h2>
+        <p className="text-blue-400 mb-4">Date: December 4, 2023</p>
+        <p className="text-gray-400">
+          Designed with beginners in mind, our CTF welcomes participants of all skill levels.
+        </p>
+        </a>
+      </div>
+      {/* Add transition and animation to the second event card */}
+      {/* <h1 className="text-4xl font-bold text-blue-300 mt-8 mb-6">Ongoing Events</h1> */}
+      {/* <a href="/events" id="events" className="text-white mt-8">See All Events</a> */}
+    </main>
+  );
 }
 
 export default Events;
