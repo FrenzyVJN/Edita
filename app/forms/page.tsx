f0b288f49d3f5fcb7b49b57550a430588cda7c25
FrenzyVJN <svijayan01@gmail.com>
2023-11-30 14:46:33 +0530

    partially mobile responsive

--- app/forms/page.tsx
+++ app/forms/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Forms() {
     return (
-    <main className='bg-gradient-to-b from-slate-950 via-slate-800 to-slate-950 animate-movingBg min-h-screen md:px-20 flex justify-center'>
+    <main className='bg-gradient-to-b from-slate-950 via-slate-800 to-slate-950 animate-movingBg min-h-screen md:px-20 flex md:justify-center'>
       <div>
       <iframe  src="https://docs.google.com/forms/d/e/1FAIpQLSfjgXl57eL6yp7fZATmboebV1XUKAvJTFAHZ2K-Pw9Ul9L_Rw/viewform?embedded=true" width="640" height="1055" className=''>Loading…</iframe>
       </div>
