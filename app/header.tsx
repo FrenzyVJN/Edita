82f7fe69cbca4686907c1f035bcc1541c65f7443
FrenzyVJN <svijayan01@gmail.com>
2023-12-31 16:51:26 +0530

    teams fix

--- app/header.tsx
+++ app/header.tsx
@@ -5,10 +5,10 @@ function Header() {
             <div className="border w-full backdrop-filter bg-white bg-opacity-10 fixed h-20 flex items-center">
                 <h1 className="text-white text-2xl md:text-6xl font-bold md:ml-10">edita</h1>
                 <div className="flex ml-auto">
-                    <Link to="home" smooth={true} duration={400} offset={-500}>
+                    <Link to="home" smooth={true} duration={300} offset={-500}>
                         <h1 className="text-white text-md mr-2 md:text-2xl md:mr-10 ml-auto font-bold hover:cursor-pointer">HOME</h1>
                     </Link>
-                    <Link to="our-team" smooth={true} duration={400} offset={-70}>
+                    <Link to="our-team" smooth={true} duration={300} offset={-80}>
                         <h1 className="text-white text-md mr-2 md:text-2xl md:mr-10 font-bold hover:cursor-pointer">TEAM</h1>
                     </Link>
                     <Link to='skills' smooth={true} duration={400} offset={-150}>
